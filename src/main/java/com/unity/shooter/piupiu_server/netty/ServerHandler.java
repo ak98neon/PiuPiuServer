2bfc8f69b1d25f66a4ef2201e1b8c2f19b807bdf
Artem Kudria <artem_kudria@epam.com>
2019-10-19 01:09:24 +0300

    Add support for REMOVE_CLIENT status

--- src/main/java/com/unity/shooter/piupiu_server/netty/ServerHandler.java
+++ src/main/java/com/unity/shooter/piupiu_server/netty/ServerHandler.java
@@ -1,31 +1,48 @@
 package com.unity.shooter.piupiu_server.netty;
 
+import com.google.gson.Gson;
+import com.unity.shooter.piupiu_server.constants.ClientStatus;
 import com.unity.shooter.piupiu_server.container.ChannelClients;
+import com.unity.shooter.piupiu_server.model.dto.ClientDataDto;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.util.CharsetUtil;
 
+import java.net.InetSocketAddress;
+
 public class ServerHandler extends SimpleChannelInboundHandler<DatagramPacket> {
     private ChannelClients channelClients;
+    private Gson gson;
 
     public ServerHandler() {
         this.channelClients = new ChannelClients();
+        this.gson = new Gson();
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, DatagramPacket o) {
-        String hostName = o.sender().getAddress().getHostAddress();
-        if (!channelClients.isAddressExist(hostName)) {
-            channelClients.addNewClient(hostName);
-            System.out.println("Add new client: " + hostName);
+        InetSocketAddress sender = o.sender();
+
+        if (!channelClients.isAddressExist(sender)) {
+            channelClients.addNewClient(sender);
+            System.out.println("Add new client: " + sender);
         }
 
         ByteBuf byteBuf = o.content();
         String in = byteBuf.toString(CharsetUtil.UTF_8);
         System.out.println(in);
-        channelHandlerContext.writeAndFlush("Response from netty: HI!");
+
+        ClientDataDto clientDataDto = gson.fromJson(in, ClientDataDto.class);
+
+        if (clientDataDto.getAction().equals(ClientStatus.REMOVE)) {
+            System.out.println("REMOVE CLIENT");
+        } else if (!clientDataDto.getAction().equals(ClientStatus.NEW_SESSION)) {
+            channelClients.sendToAllClients(channelHandlerContext, o, sender);
+        } else {
+            channelClients.startNewSession(channelHandlerContext, sender, o);
+        }
     }
 
     @Override
