bf4101d3e14c8aff955031fa25fb69456ffbfdf9
Artem Kudria <artem_kudria@epam.com>
2019-10-26 20:08:30 +0300

    Revert "Add support for REMOVE_CLIENT status"
    This reverts commit 2bfc8f69

--- src/main/java/com/unity/shooter/piupiu_server/container/ChannelClients.java
+++ src/main/java/com/unity/shooter/piupiu_server/container/ChannelClients.java
@@ -1,66 +1,33 @@
 package com.unity.shooter.piupiu_server.container;
 
-import com.google.gson.Gson;
-import com.unity.shooter.piupiu_server.model.dto.ClientDataDto;
 import com.unity.shooter.piupiu_server.util.ClientId;
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.socket.DatagramPacket;
-import io.netty.util.CharsetUtil;
 
-import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class ChannelClients {
     private ClientId clientId;
-    private Map<InetSocketAddress, String> clientsChannels = new HashMap<>();
+    private Map<String, String> clientsChannels = new HashMap<>();
 
     public ChannelClients() {
         this.clientId = new ClientId();
     }
 
-    public boolean isAddressExist(InetSocketAddress socketAddress) {
+    public boolean isAddressExist(String socketAddress) {
         return clientsChannels.containsKey(socketAddress);
     }
 
-    public void addNewClient(InetSocketAddress clientAddress) {
+    public void addNewClient(String socketAddress) {
         String id = clientId.generateUniqueId();
-        clientsChannels.put(clientAddress, id);
+        clientsChannels.put(socketAddress, id);
     }
 
-    public void startNewSession(ChannelHandlerContext channelHandlerContext, InetSocketAddress clientAddress,
-                                DatagramPacket packet) {
-        Gson gson = new Gson();
-        ByteBuf byteBuf = packet.content();
-        String in = byteBuf.toString(CharsetUtil.UTF_8);
-
-        ClientDataDto clientDataDto = gson.fromJson(in, ClientDataDto.class);
-        clientDataDto.setId(clientId.generateUniqueId());
-
-        String resp = gson.toJson(clientDataDto);
-        ByteBuf responseByte = channelHandlerContext.alloc().buffer();
-        responseByte.writeBytes(resp.getBytes());
-
-        DatagramPacket datagramPacket = new DatagramPacket(responseByte, clientAddress);
-        sendToClient(channelHandlerContext, clientAddress, datagramPacket);
+    public void sendInfoAboutNewClient() {
     }
 
-    public void sendToAllClients(ChannelHandlerContext channelHandlerContext, DatagramPacket packet,
-                                 InetSocketAddress sender) {
-        for (InetSocketAddress clientAddress : clientsChannels.keySet()) {
-            if (clientAddress != sender) {
-                sendToClient(channelHandlerContext, clientAddress, packet);
-            }
-        }
+    public void sendToAllClients() {
     }
 
-    private void sendToClient(ChannelHandlerContext channelHandlerContext, InetSocketAddress clientAddress,
-                              DatagramPacket packet) {
-        ByteBuf content = packet.content();
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(clientAddress.getHostName(), 16000);
-        DatagramPacket datagramPacket = new DatagramPacket(content, inetSocketAddress);
-        System.out.println("Send to: " + inetSocketAddress);
-        channelHandlerContext.writeAndFlush(datagramPacket);
+    public void sendToClient() {
     }
 }
--- src/main/java/com/unity/shooter/piupiu_server/netty/ServerHandler.java
+++ src/main/java/com/unity/shooter/piupiu_server/netty/ServerHandler.java
@@ -1,48 +1,31 @@
 package com.unity.shooter.piupiu_server.netty;
 
-import com.google.gson.Gson;
-import com.unity.shooter.piupiu_server.constants.ClientStatus;
 import com.unity.shooter.piupiu_server.container.ChannelClients;
-import com.unity.shooter.piupiu_server.model.dto.ClientDataDto;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.util.CharsetUtil;
 
-import java.net.InetSocketAddress;
-
 public class ServerHandler extends SimpleChannelInboundHandler<DatagramPacket> {
     private ChannelClients channelClients;
-    private Gson gson;
 
     public ServerHandler() {
         this.channelClients = new ChannelClients();
-        this.gson = new Gson();
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, DatagramPacket o) {
-        InetSocketAddress sender = o.sender();
-
-        if (!channelClients.isAddressExist(sender)) {
-            channelClients.addNewClient(sender);
-            System.out.println("Add new client: " + sender);
+        String hostName = o.sender().getAddress().getHostAddress();
+        if (!channelClients.isAddressExist(hostName)) {
+            channelClients.addNewClient(hostName);
+            System.out.println("Add new client: " + hostName);
         }
 
         ByteBuf byteBuf = o.content();
         String in = byteBuf.toString(CharsetUtil.UTF_8);
         System.out.println(in);
-
-        ClientDataDto clientDataDto = gson.fromJson(in, ClientDataDto.class);
-
-        if (clientDataDto.getAction().equals(ClientStatus.REMOVE)) {
-            System.out.println("REMOVE CLIENT");
-        } else if (!clientDataDto.getAction().equals(ClientStatus.NEW_SESSION)) {
-            channelClients.sendToAllClients(channelHandlerContext, o, sender);
-        } else {
-            channelClients.startNewSession(channelHandlerContext, sender, o);
-        }
+        channelHandlerContext.writeAndFlush("Response from netty: HI!");
     }
 
     @Override
